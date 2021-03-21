0234dcc22e1ffcca5056545f1616cb8cbda47ccd
Diana Abulebbeh <dabulebbeh@gmail.com>
2021-03-21 08:32:41 +0200

    Removing  unused code

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var bodyParser=require('body-parser');
 var app=express();
 var port =process.env.port || 4000;
 const mongoose = require('mongoose')
-const uri = 'mongodb://localhost/abcCompany2'
+const uri = 'mongodb://localhost/abcCompany'
  mongoose.createConnection(uri)
 mongoose.connect(uri);
 var db =mongoose.connection;
@@ -25,42 +25,3 @@ app.use(function(req,res,next){
 app.listen(port);
 console.log("server start")
 
-
-
-
-
-
-
-// const test = new AddUser({ username: 'Diana', password: "diana123" ,admin:true})
-// const testComplaint = new AddComplaint({ message: 'new Complaint', status: "pending" })
-
-// const Schema = mongoose.Schema
-// const users = new Schema({
-//     username: { type: String, required: true, unique: true },
-//     password: { type: String, required: true },
-//     admin: Boolean,
-// })
-
-// const complaint = new Schema({
-//     message: { type: String,  required: true,},
-//     status: { type: String },
-   
-// })
-// const User = db.model('users', users)
-// const Complaint = db.model('complaint', complaint)
-
-
-// db.once('connected', function (err) {
-//   if (err) { return console.error(err) }
-//   AddUser.create(test, function (err, doc) {
-//     if (err) { return console.error(err) }
-//     console.log(doc)
-//     return db.close()
-//   }) 
-
-//   AddComplaint.create(testComplaint, function (err, doc) {
-//     if (err) { return console.error(err) }
-//     console.log(doc)
-//     return db.close()
-//   }) 
-// })
